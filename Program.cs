c2f53762b358cccc74ffcbaa6196273e1460d617
Yusukeirinel <Yusukeirinel@gmail.com>
2020-03-11 14:24:59 +0200

    a1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 24242424;
+            long num2 = num1;
+            float num3 = 3.454f;
+            double num4 = 4.4543534535345435345345546456456456456456547474574574574574574574574574353534534534D;
+            double num5 = num3;
+            double num6 = 34.5555;
+            Console.WriteLine(num3);
+            Console.Read();
+        }
+    }
+}
